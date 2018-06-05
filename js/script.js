5b4e247b57e7c9e5c4c57de103e20adc25f8cf0b
ericzprice <ericzprice@gmail.com>
2018-06-05 15:55:29 -0600

    Fading animation

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+  $(".red-box").fadeOut(2000).fadeIn(1000).fadeTo(1000, 0.25);
+  $(".green-box").fadeOut(2000).fadeIn(1000).fadeTo(1000, 0.50);
+  $(".blue-box").fadeOut(2000).fadeIn(1000).fadeTo(1000, 0.80);
 });
